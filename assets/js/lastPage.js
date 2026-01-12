0af46a576a7b8936c957c790590c4ff57814c145
Aleksandr <a.lorer@dss.kz>
2026-01-12 11:35:14 +0500

    исправлено LastPage.js

--- assets/js/lastPage.js
+++ assets/js/lastPage.js
@@ -3,26 +3,18 @@ class LastPage {
         this.defaultPageId = "status";
         this.varName = "lastPage";
 
-        if (!sessionStorage.getItem(this.varName))
+        if (!sessionStorage.getItem(this.varName)) {
             sessionStorage.setItem(this.varName, this.defaultPageId);
+        }
     }
 
-    /** Store last page ID
-     *
-     * @param {string} pageId 
-     */
     set(pageId) {
         sessionStorage.setItem(this.varName, pageId);
     }
 
-    /** Get last page ID
-     *
-     * @returns {string} pageId
-     */
     get() {
         return sessionStorage.getItem(this.varName);
     }
-};
+}
 
-module.exports = LastPage;
 globalThis.LastPage = LastPage;
